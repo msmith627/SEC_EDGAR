c53fc0428a9728f3030991b3180bbb12d74baaf0
msmith627 <113635399+msmith627@users.noreply.github.com>
2023-07-28 16:37:02 -0600

    Update xBRLParser.py

--- xBRLParser.py
+++ xBRLParser.py
@@ -7,122 +7,152 @@ import json
 import xmltojson
 warnings.filterwarnings("ignore")
 
-# set header parameters required by sec request
-User_Agent = "m_smith627@hotmail.com"
-Accept_Encoding = "gzip, deflate"
-Host = "www.sec.gov"
 
-headers_txt = {
-    "User-Agent" : User_Agent,
-    "Accept-Encoding" : Accept_Encoding,
-    "Host" : Host
-}
+def retrieve_fileurls():
 
-# URLList = ['https://www.sec.gov/Archives/edgar/data/1730984/000173098423000064/0001730984-23-000064-index.htm',
-#            'https://www.sec.gov/Archives/edgar/data/6207/000141057823001494/0001410578-23-001494-index.htm',
-#            'https://www.sec.gov/Archives/edgar/data/1117228/000149315223025495/0001493152-23-025495-index.htm',
-#            'https://www.sec.gov/Archives/edgar/data/16160/000156276223000287/0001562762-23-000287-index.htm',
-#            'https://www.sec.gov/Archives/edgar/data/1862993/000110465923083813/0001104659-23-083813-index.htm',
-#            'https://www.sec.gov/Archives/edgar/data/1679273/000155837023012203/0001558370-23-012203-index.htm',
-#            'https://www.sec.gov/Archives/edgar/data/1852016/000119312523193294/0001193125-23-193294-index.htm',
-#            'https://www.sec.gov/Archives/edgar/data/1084765/000108476523000016/0001084765-23-000016-index.htm',
-#            'https://www.sec.gov/Archives/edgar/data/771856/000162828023025464/0001628280-23-025464-index.htm',
-#            'https://www.sec.gov/Archives/edgar/data/1339005/000114036123035663/0001140361-23-035663-index.htm',
-#            'https://www.sec.gov/Archives/edgar/data/1892747/000149315223025239/0001493152-23-025239-index.htm',
-#            'https://www.sec.gov/Archives/edgar/data/866729/000086672923000019/0000866729-23-000019-index.htm',
-#            'https://www.sec.gov/Archives/edgar/data/940944/000094094423000037/0000940944-23-000037-index.htm',
-#            'https://www.sec.gov/Archives/edgar/data/1622996/000164033423001347/0001640334-23-001347-index.htm',
-#            'https://www.sec.gov/Archives/edgar/data/915358/000091535823000008/0000915358-23-000008-index.htm']
-#
-#
-# for url in URLList:
+    # set header parameters required by sec request
+    User_Agent = "m_smith627@hotmail.com"
+    Accept_Encoding = "gzip, deflate"
+    Host = "www.sec.gov"
 
-# return the cik from the URL
-FedURL = url
-cik = FedURL.split('/')[6]
-# print(URL)
+    headers_txt = {
+        "User-Agent": User_Agent,
+        "Accept-Encoding": Accept_Encoding,
+        "Host": Host
+    }
 
-# details on issuer, file type and date of document.
-# cik = '' # cik for International business machines corp (IBM)
-type = '10-k' # annual filing
-dateb = '' # 2016
+    # URLList = ['https://www.sec.gov/Archives/edgar/data/1730984/000173098423000064/0001730984-23-000064-index.htm',
+    #            'https://www.sec.gov/Archives/edgar/data/6207/000141057823001494/0001410578-23-001494-index.htm',
+    #            'https://www.sec.gov/Archives/edgar/data/1117228/000149315223025495/0001493152-23-025495-index.htm',
+    #            'https://www.sec.gov/Archives/edgar/data/16160/000156276223000287/0001562762-23-000287-index.htm',
+    #            'https://www.sec.gov/Archives/edgar/data/1862993/000110465923083813/0001104659-23-083813-index.htm',
+    #            'https://www.sec.gov/Archives/edgar/data/1679273/000155837023012203/0001558370-23-012203-index.htm',
+    #            'https://www.sec.gov/Archives/edgar/data/1852016/000119312523193294/0001193125-23-193294-index.htm',
+    #            'https://www.sec.gov/Archives/edgar/data/1084765/000108476523000016/0001084765-23-000016-index.htm',
+    #            'https://www.sec.gov/Archives/edgar/data/771856/000162828023025464/0001628280-23-025464-index.htm',
+    #            'https://www.sec.gov/Archives/edgar/data/1339005/000114036123035663/0001140361-23-035663-index.htm',
+    #            'https://www.sec.gov/Archives/edgar/data/1892747/000149315223025239/0001493152-23-025239-index.htm',
+    #            'https://www.sec.gov/Archives/edgar/data/866729/000086672923000019/0000866729-23-000019-index.htm',
+    #            'https://www.sec.gov/Archives/edgar/data/940944/000094094423000037/0000940944-23-000037-index.htm',
+    #            'https://www.sec.gov/Archives/edgar/data/1622996/000164033423001347/0001640334-23-001347-index.htm',
+    #            'https://www.sec.gov/Archives/edgar/data/915358/000091535823000008/0000915358-23-000008-index.htm']
+    #
+    #
+    # for url in URLList:
 
-# cik = '0000051143' # cik for International business machines corp (IBM)
-# type = '10-K' # annual filing
-# dateb = '20231231' # 2016
+    # return the cik from the URL
+    FedURL = 'https://www.sec.gov/Archives/edgar/data/1730984/000173098423000064/0001730984-23-000064-index.htm'
+    # FedURL = url
+    cik = FedURL.split('/')[6]
+    # print(URL)
 
-# base URL for company filings search
-base_url = "https://www.sec.gov/cgi-bin/browse-edgar?action=getcompany&CIK={}&type={}&dateb={}"
-format_url = base_url.format(cik, type, dateb)
-# use requests .get method
-edgar_resp = requests.get(base_url.format(cik, type, dateb), headers=headers_txt)
-edgar_str = edgar_resp.text
-# print(f'BASE URL : {format_url}')
-# print(f'EDGAR STR URL : {edgar_str}')
-doc_link = ''
-soup = BeautifulSoup(edgar_str, 'html.parser')
-table_tag = soup.find('table', class_='tableFile2')
-rows = table_tag.find_all('tr')
+    # details on issuer, file type and date of document.
+    # cik = '' # cik for International business machines corp (IBM)
+    type = '10-k' # annual filing
+    dateb = '' # 2016
 
-# print(table_tag)
-print ("done")
+    # cik = '0000051143' # cik for International business machines corp (IBM)
+    # type = '10-K' # annual filing
+    # dateb = '20231231' # 2016
 
-doc_link_list = []
+    # base URL for company filings search
+    base_url = "https://www.sec.gov/cgi-bin/browse-edgar?action=getcompany&CIK={}&type={}&dateb={}"
+    format_url = base_url.format(cik, type, dateb)
+    # use requests .get method
+    edgar_resp = requests.get(base_url.format(cik, type, dateb), headers=headers_txt)
+    edgar_str = edgar_resp.text
+    # print(f'BASE URL : {format_url}')
+    # print(f'EDGAR STR URL : {edgar_str}')
+    doc_link = ''
+    soup = BeautifulSoup(edgar_str, 'html.parser')
+    table_tag = soup.find('table', class_='tableFile2')
+    rows = table_tag.find_all('tr')
 
-for row in rows :
-    cells = row.find_all('td')
-    if len(cells) > 3 :
-        if cells[3].text > '2015-01-01':
-        #if '10-k' in cells[1].text :
-            doc_link = 'https://www.sec.gov' + cells[1].a['href']
-            doc_link_list.append(doc_link)
-print(doc_link_list)
+    # print(table_tag)
+    # print("done")
 
-# filingURL = 'https://www.sec.gov/Archives/edgar/data/1730346/000173034623000030/0001730346-23-000030-index.htm'
-#              https://www.sec.gov/Archives/edgar/data/1886591/000119312523193225/0001193125-23-193225-index.htm
-# doc_link = filingURL
-
-# Exit if document link couldn't be found
-if doc_link == '' :
-    print("Couldn't find the document link")
-    sys.exit()
-
-
-for i in doc_link_list:
-
-    # Obtain HTML for document page
-    print(f'DOC Link : {i}')
-    doc_resp = requests.get(i, headers=headers_txt)
-    doc_str = doc_resp.text
-
-    # Find the XBRL link
-    xbrl_link = ''
-    soup = BeautifulSoup(doc_str, 'html.parser')
-    # skip the processing if the company has not filed the xbrl data files
-    if soup.find('table', class_='tableFile', summary='Data Files') != None:
-        table_tag = soup.find('table', class_='tableFile', summary='Data Files')
-        rows = table_tag.find_all('tr')
-    else:
-        print('Passing: ' + i)
-        pass
+    doc_link_list = []
 
     for row in rows :
         cells = row.find_all('td')
-        if len(cells) > 3:
-            if 'INS' in cells[1].text:
-                xbrl_link = 'https://www.sec.gov' + cells[2].a['href']
-                print(xbrl_link)
+        if len(cells) > 3 :
+            if cells[3].text > '2015-01-01':
+            #if '10-k' in cells[1].text :
+                doc_link = 'https://www.sec.gov' + cells[1].a['href']
+                doc_link_list.append(doc_link)
+    print(doc_link_list)
+
+    # filingURL = 'https://www.sec.gov/Archives/edgar/data/1730346/000173034623000030/0001730346-23-000030-index.htm'
+    #              https://www.sec.gov/Archives/edgar/data/1886591/000119312523193225/0001193125-23-193225-index.htm
+    # doc_link = filingURL
+
+    # Exit if document link couldn't be found
+    if doc_link == '':
+        print("Couldn't find the document link")
+        sys.exit()
+
+    for i in doc_link_list:
+
+        # Obtain HTML for document page
+        print(f'DOC Link : {i}')
+        doc_resp = requests.get(i, headers=headers_txt)
+        doc_str = doc_resp.text
+
+        # Find the XBRL links
+        ins_link = ''
+        sch_link = ''
+        cal_link = ''
+        def_link = ''
+        lab_link = ''
+        pre_link = ''
+        soup = BeautifulSoup(doc_str, 'html.parser')
+        # skip the processing if the company has not filed the xbrl data files
+        if soup.find('table', class_='tableFile', summary='Data Files') != None:
+            table_tag = soup.find('table', class_='tableFile', summary='Data Files')
+            rows = table_tag.find_all('tr')
+        else:
+            print('Passing: ' + i)
+            pass
+
+        for row in rows :
+            cells = row.find_all('td')
+            if len(cells) > 3:
+                if 'INS' in cells[1].text:
+                    # Instance docs seem to contain the context of the filings
+                    ins_link = 'https://www.sec.gov' + cells[2].a['href']
+                    print(ins_link)
+                elif 'SCH' in cells[1].text:
+                    # DO NOT KNOW what schema files contain yet
+                    sch_link = 'https://www.sec.gov' + cells[2].a['href']
+                    print(sch_link)
+                elif 'CAL' in cells[1].text:
+                    # DO NOT KNOW what calculation files contain yet
+                    cal_link = 'https://www.sec.gov' + cells[2].a['href']
+                    print(cal_link)
+                elif 'DEF' in cells[1].text:
+                    # DO NOT KNOW what definition files contain yet
+                    def_link = 'https://www.sec.gov' + cells[2].a['href']
+                    print(def_link)
+                elif 'LAB' in cells[1].text:
+                    # DO NOT KNOW what label files contain yet
+                    lab_link = 'https://www.sec.gov' + cells[2].a['href']
+                    print(lab_link)
+                elif 'PRE' in cells[1].text:
+                    # DO NOT KNOW what presentation files contain yet
+                    pre_link = 'https://www.sec.gov' + cells[2].a['href']
+                    print(pre_link)
+    return
 
 
 
-    # Obtain XBRL text from document
-    print(f'XBRL Link : {xbrl_link}')
-    xbrl_resp = requests.get(xbrl_link, headers=headers_txt)
-    xbrl_str = xbrl_resp.text
+    # Obtain INS text from document
+    print(f'INS Link : {ins_link}')
+    ins_resp = requests.get(ins_link, headers=headers_txt)
+    ins_str = ins_resp.text
 
 
     # Find and print stockholder's equity
-    soup = BeautifulSoup(xbrl_str, 'lxml')
+    soup = BeautifulSoup(ins_str, 'lxml')
     if soup.find_all(name='xbrli:context').__len__() > 0:
         # process context tags pre-2016
         tag_list = soup.find_all(name='xbrli:context')
@@ -130,8 +160,8 @@ for i in doc_link_list:
         # process context tags 2016 and greater
         tag_list = soup.find_all(name='context')
 
-    # print(xbrl_str)
-    # json = json.loads(xbrl_str)
+    # print(ins_str)
+    # json = json.loads(ins_str)
     # print(json)
 
 # This section of code creates a context table.
@@ -146,7 +176,7 @@ for i in doc_link_list:
     tag_list_root = '<root>' + str(tag_list) + '</root>'
 
 
-    #
+    #Encode/Serialize the JSON
     with open("Contexts.html", "w") as html_file:
         html_file.write(str(tag_list_root))
 
@@ -158,10 +188,10 @@ for i in doc_link_list:
     accessionNumber = i.split('/')[7]
     # print(accessionNumber[7])
 
-    with open("JSONFile" + accessionNumber + ".json", "w") as json_file:
+    with open("JSONInsFile" + accessionNumber + ".json", "w") as json_file:
         json_file.write(jsonstr)
 
-    print('created: JSONFile' + accessionNumber + '.json')
+    print('created: JSONInsFile' + accessionNumber + '.json')
 
 
     # print(jsonstr)
